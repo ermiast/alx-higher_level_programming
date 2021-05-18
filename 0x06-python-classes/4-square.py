0bf233ac69c109e0b20dce152348f25c2134b00d
ermiast <ligachayo@gmail.com>
2021-05-18 20:51:47 +0000

    access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,34 +1,27 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class
+Its implements value and type checks for its attributes with area function
+"""
+
+
 class Square:
-    """ class square defines a square by size:
-    size must be an integer
-    size must not be negative """
+    """Square implementation
+    """
     def __init__(self, size=0):
-        """ initialize square objects,
-        sets size equal to 0 by default,
-        checks if size has the correct type and value """
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.size = size
 
     @property
     def size(self):
-        """ gets the value of __size """
         return self.__size
 
     @size.setter
-    def size(self, value):
-        """ sets the value of __size """
-        if type(value) is not int:
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
 
     def area(self):
-        """ returns the area of the square """
-        return self.__size ** 2
+        return (self.__size ** 2)
